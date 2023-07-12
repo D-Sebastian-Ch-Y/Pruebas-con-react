db26208a715bcbe98be1576ba71c258ca53cb81d
David Sebastian <david.champi.y@gmail.com>
2023-07-12 18:27:09 -0500

    Usando datos js en los componentes

--- react_fs_test/part1/src/index.js
+++ react_fs_test/part1/src/index.js
@@ -4,18 +4,23 @@ import ReactDOM from 'react-dom'
 const Hello = (props) => {
   return (
     <div>
-      <p>Buenas {props.name}</p>
+      <p>Buenas {props.name}, tienes {props.age} años de edad ¿verdad?</p>
     </div>
   )
 }
 
 const App = () => {
+
+  const name=["Johan","Leo"]
+  const age = ["21","22-1"]
+
   return (
     <div>
-      <h1>Saludos:</h1>
-      <Hello name="Johan"/>
+      <h1>Saludos, mi estimado:</h1>
       <br />
-      <Hello name="Marco"/>
+      <Hello name="Marco" age={10+12}/>
+      <Hello name={name[0]} age={age[0]}/>
+      <Hello name={name[1]} age={age[1]}/>
     </div>
   )
 }
